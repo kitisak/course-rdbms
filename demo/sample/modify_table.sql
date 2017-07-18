4095d7ddb459d544d5d105fe51d6205fb83822c5
somkiat <somkiat.p@gmail.com>
2017-07-18 09:55:00 +0700

    Workshop modify table script

--- demo/sample/modify_table.sql
+++ demo/sample/modify_table.sql
@@ -14,7 +14,48 @@ CHANGE amount amount2 int;
 ALTER TABLE summary
 ADD constraint check (amount2 >= 0);
 
+//Add table constraint
+// 1. Add Primary KEY
+ALTER TABLE summary
+ADD PRIMARY KEY(isbn);
 
+// 2. Add FOREIGN KEY
+ALTER TABLE summary
+ADD CONSTRAINT fk_isbn
+FOREIGN KEY (isbn) REFERENCES book(isbn);
+
+
+CREATE TABLE book(
+  isbn varchar(20) primary key
+);
+
+// 3. Add INDEX
+ALTER TABLE `table`
+ADD INDEX `index nane` (`product_id`)
+
+// Delete table constraint
+
+// 1. Delete Primary KEY
+ALTER TABLE summary
+DROP PRIMARY KEY;
+
+// 2. Delete FOREIGN KEY
+ALTER TABLE summary
+DROP FOREIGN KEY fk_isbn;
+
+// 3. Delete INDEX
+ALTER TABLE `table_name`
+DROP INDEX  `id_name_fk`;
+
+// Additional
+ALTER TABLE  summary
+DROP PRIMARY KEY ,
+ADD PRIMARY KEY (  isbn );
+
+ALTER TABLE  summary
+ADD PRIMARY KEY(isbn),
+ADD CONSTRAINT fk_isbn
+FOREIGN KEY (isbn) REFERENCES book(isbn);
 
 //Show detail of table
 desc summary;
